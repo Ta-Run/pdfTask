ab30935fea9db0b2289983ec0f7bb265e3c1e813
Ta-Run <tarnsharma11091999@gmail.com>
2023-11-03 01:18:53 +0530

    resolve pdf upload  issue

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,44 +1,45 @@
 "use client"
 import { useState } from 'react';
-import { Document, Page,pdfjs  } from 'react-pdf';
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
 
-
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.js',
-    import.meta.url,
-  ).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`;
 
 function PdfHome() {
-  const [numPages, setNumPages] = useState<number|undefined>();
-  const [pageNumber, setPageNumber] = useState<number|any>(1);
+  const [numPages, setNumPages] = useState<number | undefined>();
+  const [pageNumber, setPageNumber] = useState<number | any>(1);
+  const [selectedFile, setSelectedFile] = useState<File | null | any>(null);
 
-  console.log(pageNumber)
   function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
-    console.log('hello');
-    console.log(numPages)
     setNumPages(numPages);
   }
-  console.log(numPages)
+
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files) {
+      const file = files[0];
+      // const fileURL = URL.createObjectURL(file); // Convert File to URL
+      setSelectedFile(file);
+    }
+  }
 
   return (
-    <div>
-    <Document file="https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-      <Page pageNumber={pageNumber} />
-    </Document>
-    {numPages !== undefined && (
-      <div>
-        {Array.from(
-          new Array(numPages),
-          (el, index) => (
-            <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-          )
-        )}
+    <div className='bg-blue-500 md:bg-green-500 '>
+      <div className="flex justify-around  ,shadow hover:shadow-lg" >
+        <form >
+          <input type="file" onChange={(e) => handleFileChange(e)} />
+        </form>
       </div>
-    )}
-    <p>
-      Page {pageNumber} of {numPages || 0}
-    </p>
-  </div>
+      <div className="flex justify-around ">
+        <Document file={selectedFile} onLoadSuccess={onDocumentLoadSuccess}>
+          <Page className="py-12" devicePixelRatio={1} pageNumber={pageNumber} renderTextLayer={false} renderAnnotationLayer={false} scale={1.5} loading={"Loading page…"} />
+        </Document>
+      </div>
+      <div className="flex justify-around ">
+        Page {pageNumber} of {numPages || 0}
+      </div>
+    </div>
   );
 }
 export default PdfHome;
